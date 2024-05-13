e1dd90e0f25188f0bba6c8fb0654d11438456432
anisenson <adamjn524@gmail.com>
2024-05-13 10:17:44 -0400

    Added ToDO function

--- js/app.js
+++ js/app.js
@@ -18,3 +18,20 @@ const today = new Date();
 dateElement.innerHTML = today.toLocaleDateString("en-us", options);
 
 // Add a to do function
+
+function addToDo(toDo){
+
+    const item = `
+            <li class="item">
+                <i class="fa fa-circle-thin co" job="complete" id="0"></i>
+                <p class="text">${toDo}</p>
+                <i class="fa fa-trash-o de" job="delete" id="0"></i>
+            </li>
+            `;
+
+    const position = "beforeend";
+
+    list.insertAdjacentHTML(position, item);
+}
+
+addToDo("drink Cofee")
