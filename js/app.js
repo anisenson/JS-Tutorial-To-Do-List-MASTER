94af11bd07b49e8cfdd58c7192741ab4b327f339
anisenson <adamjn524@gmail.com>
2024-05-13 10:11:03 -0400

    Added HTML and Date

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 // CODE EXPLAINED channel
+
+// Select the Elements
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+// Classes Names
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+// Show Todays Date
+const options = {weekday : "long", month:"short", day:"numeric"};
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString("en-us", options);
+
+// Add a to do function
